4881b58e73fb61b2564e7357b157b446e9ed0236
21kosa <aikpitanyi.iyegbekosa@gmail.com>
2025-06-09 12:56:54 +0100

    user details url updated

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('register_driver', CustomDriverCreate.as_view(), name='register_driver'),
     path('login_user', RiderLoginView.as_view(), name='login_user'),
     path('login_driver', DriverLoginView.as_view(), name='login_driver'),
-    path('user/profile/', UserProfileView.as_view(), name='user_profile'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
 ]
